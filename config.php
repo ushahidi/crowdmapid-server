4cdbd9591725cbad06482ec9f24972a6979c0d60
Evan Sims <evan@ushahidi.com>
2012-02-28 00:17:47 -0600

    First commit.

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+	if(!defined('LOADED_SAFELY')) die('You cannot access this file directly.');
+
+	// Enable for troubleshooting. Disable for improved performance.
+	define(    'CFG_DEBUG_ENV',  true );
+
+	define(         'CFG_SALT',  '' );
+	define(         'CFG_NAME',  'CrowdmapID' );
+	define(          'CFG_URL',  'http://crowdmapid.com' );
+
+	define(     'CFG_SQL_HOST',  '127.0.0.1' );
+	define(     'CFG_SQL_USER',  'root' );
+	define( 'CFG_SQL_PASSWORD',  'root' );
+	define( 'CFG_SQL_DATABASE',  'riverid' );
+
+    define('CFG_RATELIMIT_SEC',  3600 );
